428cdd55970b07964ef99e3fa7a35f1af798bb53
Viktor <vampirit@mail.ru>
2017-11-29 00:50:37 +0400

    print time after each test

--- src/test/java/ru/javawebinar/topjava/TimerTest.java
+++ src/test/java/ru/javawebinar/topjava/TimerTest.java
@@ -5,17 +5,23 @@ import org.junit.AfterClass;
 import org.junit.Rule;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.Description;
+import org.slf4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 public class TimerTest {
+    private static final Logger LOG = getLogger(TimerTest.class);
+
     private static String className;
     private static Map<String, Long> testTimer = new HashMap<>();
     @Rule
     public Stopwatch timer = new Stopwatch() {
         @Override
         protected void finished(long nanos, Description description) {
+            LOG.info("{} - {} ms", description.getMethodName(), nanos/1000000);
             testTimer.put(description.getTestClass().getSimpleName()+"."+description.getMethodName(), nanos/1000000);
             className = description.getTestClass().getSimpleName();
         }
