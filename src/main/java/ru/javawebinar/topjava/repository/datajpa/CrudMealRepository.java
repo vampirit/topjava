c106ccaf7d6324d983491fcb369a62e23de0d32f
Viktor <vampirit@mail.ru>
2017-12-04 01:41:49 +0400

    implement data jpa meal repository

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,21 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+    List<Meal> findAllByUser_IdOrderByDateTimeDesc(int userId);
+
+    Optional<Meal> findByIdAndUser_Id(int id, int userId);
+
+    @Transactional
+    int deleteByIdAndUser_Id(int id, int userId);
+
+    List<Meal> findAllByUser_IdAndDateTimeBetweenOrderByDateTimeDesc(int userId, LocalDateTime start, LocalDateTime end);
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -3,39 +3,54 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class DataJpaMealRepositoryImpl implements MealRepository {
+
+    @PersistenceContext
+    private EntityManager em;
 
     @Autowired
     private CrudMealRepository crudRepository;
 
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    public Meal save(Meal meal, int userId) {
+        User reference = em.getReference(User.class, userId);
+        meal.setUser(reference);
+
+        if (!meal.isNew() && get(meal.getId(), userId) == null){
+            return null;
+        }
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.deleteByIdAndUser_Id(id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Optional<Meal> byId = crudRepository.findByIdAndUser_Id(id, userId);
+        return byId.orElse(null);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+
+        return crudRepository.findAllByUser_IdOrderByDateTimeDesc(userId);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return crudRepository.findAllByUser_IdAndDateTimeBetweenOrderByDateTimeDesc(userId, startDate, endDate);
     }
 }
