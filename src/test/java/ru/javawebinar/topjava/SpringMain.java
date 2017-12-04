54432d4bba8ba79686dc4db1efffcafb98a231d4
Viktor <vampirit@mail.ru>
2017-12-04 20:17:03 +0400

    fix SpringMain with parent context

--- src/test/java/ru/javawebinar/topjava/SpringMain.java
+++ src/test/java/ru/javawebinar/topjava/SpringMain.java
@@ -2,6 +2,8 @@ package ru.javawebinar.topjava;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.StandardEnvironment;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.to.MealWithExceed;
@@ -17,18 +19,26 @@ import java.util.List;
 public class SpringMain {
     public static void main(String[] args) {
         // java 7 Automatic resource management
-        try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml","spring/mock.xml")) {
-            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
-            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
-            adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
-            System.out.println();
+        try(ConfigurableApplicationContext parentCtx = new ClassPathXmlApplicationContext()) {
+            ConfigurableEnvironment environment = parentCtx.getEnvironment();
+            environment.addActiveProfile(Profiles.POSTGRES_DB);
+            environment.addActiveProfile(Profiles.DATAJPA);
+            parentCtx.refresh();
 
-            MealRestController mealController = appCtx.getBean(MealRestController.class);
-            List<MealWithExceed> filteredMealsWithExceeded =
-                    mealController.getBetween(
-                            LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
-                            LocalDate.of(2015, Month.MAY, 31), LocalTime.of(11, 0));
-            filteredMealsWithExceeded.forEach(System.out::println);
+            try (ConfigurableApplicationContext appCtx = new ClassPathXmlApplicationContext(new String[]{"spring/spring-app.xml", "spring/spring-db.xml"}, parentCtx)) {
+                System.out.println("Bean definition names: ");
+                Arrays.stream(appCtx.getBeanDefinitionNames()).forEachOrdered(System.out::println);
+                AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+                adminUserController.create(new User(null, "userName", "email", "password", Role.ROLE_ADMIN));
+                System.out.println();
+
+                MealRestController mealController = appCtx.getBean(MealRestController.class);
+                List<MealWithExceed> filteredMealsWithExceeded =
+                        mealController.getBetween(
+                                LocalDate.of(2015, Month.MAY, 30), LocalTime.of(7, 0),
+                                LocalDate.of(2015, Month.MAY, 31), LocalTime.of(13, 0));
+                filteredMealsWithExceeded.forEach(System.out::println);
+            }
         }
     }
 }
