549e090750bca1cc3671a3b3c23d44d0d7ab5102
Viktor <vampirit@mail.ru>
2017-12-27 17:24:26 +0400

    Реализовать MealRestController и протестировать его через MealRestControllerTest

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,7 +1,50 @@
 package ru.javawebinar.topjava.web.meal;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.to.MealWithExceed;
 
-@Controller
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE_TIME;
+
+@RestController
+@RequestMapping(MealRestController.REST_URL)
 public class MealRestController extends AbstractMealController {
+    static final String REST_URL = "/rest/meals";
+
+    @DeleteMapping(value = "/{id}")
+    public void deleteMeal(@PathVariable("id") int id) {
+        super.delete(id);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MealWithExceed> getAllMeal() {
+        return super.getAll();
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Meal> createWithLocation(@RequestBody Meal meal) {
+
+        Meal createdMeal = super.create(meal);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdMeal);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void updateMeal(@RequestBody Meal meal, @PathVariable("id") int id) {
+        super.update(meal, id);
+    }
+
+    @PostMapping(value = "/between", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.TEXT_HTML_VALUE)
+    public List<MealWithExceed> getBetweenDateTime(
+                           @DateTimeFormat(iso = DATE_TIME) @RequestParam LocalDateTime start,
+                           @DateTimeFormat(iso = DATE_TIME) @RequestParam LocalDateTime end) {
+        return super.getBetween(start.toLocalDate(), start.toLocalTime(), end.toLocalDate(), end.toLocalTime());
+    }
 }
---
+++ src/test/java/ru/javawebinar/topjava/web/meal/MealRestControllerTest.java
@@ -0,0 +1,114 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.TestUtil;
+import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.AbstractControllerTest;
+
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.web.json.JsonUtil.writeValue;
+
+public class MealRestControllerTest extends AbstractControllerTest {
+
+    private static final String REST_URL = MealRestController.REST_URL + '/';
+
+
+    @Autowired
+    protected MealService mealService;
+
+    @Test
+    public void deleteMeal() throws Exception {
+        mockMvc.perform(delete(REST_URL + MEAL1.getId()))
+                .andDo(print())
+                .andExpect(status().isOk());
+        List<Meal> meals = new ArrayList<>(MEALS);
+        meals.remove(MEAL1);
+        assertMatch(mealService.getAll(UserTestData.USER_ID), meals);
+    }
+
+    @Test
+    public void getAll() throws Exception {
+        List<MealWithExceed> withExceeded = MealsUtil.getWithExceeded(mealService.getAll(AuthorizedUser.id()), AuthorizedUser.getCaloriesPerDay());
+        mockMvc.perform(get(REST_URL))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(writeValue(withExceeded)));
+
+    }
+
+    @Test
+    public void createWithLocation() throws Exception {
+        Meal created = MealTestData.getCreated();
+        ResultActions resultActions = mockMvc.perform(post(REST_URL)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(writeValue(created)))
+                .andDo(print())
+                .andExpect(status().isCreated());
+
+        Meal meal = TestUtil.readFromJson(resultActions, Meal.class);
+
+        List<Meal> meals = new ArrayList<>(MEALS);
+        meals.add(0, meal);
+        assertMatch(mealService.getAll(UserTestData.USER_ID), meals);
+    }
+
+    @Test
+    public void update() throws Exception {
+
+        Meal updated = MealTestData.getUpdated();
+        mockMvc.perform(put(REST_URL + updated.getId())
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(writeValue(updated)))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        List<Meal> meals = new ArrayList<>(MEALS);
+        for (int i = 0; i < meals.size(); i++) {
+            if (meals.get(i).getId().equals(updated.getId()))
+                meals.set(i, updated);
+        }
+
+        assertMatch(mealService.getAll(UserTestData.USER_ID), meals);
+    }
+
+    @Test
+    public void getBetweenDateTime() throws Exception {
+        LocalDateTime start = LocalDateTime.of(2015, Month.MAY, 30, 10, 0);
+        LocalDateTime end = LocalDateTime.of(2015, Month.MAY, 31, 13, 0);
+
+        List<MealWithExceed> filteredWithExceeded = MealsUtil.getFilteredWithExceeded(
+                mealService.getBetweenDates(start.toLocalDate(), end.toLocalDate(), AuthorizedUser.id()),
+                start.toLocalTime(), end.toLocalTime(), AuthorizedUser.getCaloriesPerDay());
+
+        mockMvc.perform(post(REST_URL + "/between")
+                .contentType(MediaType.TEXT_HTML_VALUE)
+                .param("start", start.toString())
+                .param("end", end.toString()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().json(writeValue(filteredWithExceeded)));
+
+
+    }
+
+}
